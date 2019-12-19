39e07015f2217d8d6a8968194f25745d2cd748d2
Zhenbo Zhang <zhenbo.zhang@microsoft.com>
2019-12-19 22:03:54 +0200

    test20

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,7 @@
-$templateFile = "$env:ARTIFACT_DIR\Drop\template.json"
-$templateParameterFile = "$env:ARTIFACT_DIR\Drop\generated.parameters.json"
+$templateFile = "$env:ARTIFACT_DIR\$env:PRIMARY_ARTIFACT_SOURCE_ALIAS\template.json"
+$templateParameterFile = "$env:ARTIFACT_DIR\$env:PRIMARY_ARTIFACT_SOURCE_ALIAS\generated.parameters.json"
 $resourceGroupName = "zhenzh-arm-demo1"
 $location = "West Europe"
-
-Write-Host "arc source: $env:PRIMARY_ARTIFACT_SOURCE_ALIAS"
 
 Write-Host "Content in directory: $env:ARTIFACT_DIR"
 Get-ChildItem -Path $env:ARTIFACT_DIR -Depth 4
