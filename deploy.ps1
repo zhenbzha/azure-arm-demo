5306222a72d2d285d642c81859631a66db386383
Zhenbo Zhang <zhenbo.zhang@microsoft.com>
2019-12-19 21:53:40 +0200

    test18

--- deploy.ps1
+++ deploy.ps1
@@ -1,12 +1,10 @@
-Write-Host "hello world"
-Write-Host "No problem reading $env:ARTIFACT_DIR"
-Get-ChildItem -Path $env:ARTIFACT_DIR -Depth 4
-Write-Host "hello world"
-
-$templateFile = "$env:ARTIFACT_DIR\Drop\azure-arm-demo\template.json"
-$templateParameterFile = "$env:ARTIFACT_DIR\Drop\azure-arm-demo\generated.parameters.json"
+$templateFile = "$env:ARTIFACT_DIR\Drop\template.json"
+$templateParameterFile = "$env:ARTIFACT_DIR\Drop\generated.parameters.json"
 $resourceGroupName = "zhenzh-arm-demo1"
 $location = "West Europe"
+
+Write-Host "Content in directory: $env:ARTIFACT_DIR"
+Get-ChildItem -Path $env:ARTIFACT_DIR -Depth 4
 
 New-AzResourceGroup `
     -Name $resourceGroupName `
