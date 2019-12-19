052367a1ec20dc20eb8aeef2dc5bdf9f88468382
Zhenbo Zhang <zhenbo.zhang@microsoft.com>
2019-12-19 17:46:30 +0200

    use artifact dir

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,10 @@
-$templateUri = "https://zhenzhupload.blob.core.windows.net/fileupload/template.json?st=2019-12-19T14%3A57%3A22Z&se=2020-12-31T14%3A57%3A00Z&sp=rl&sv=2018-03-28&sr=b&sig=fBmzjvE4z5crIPs57jW5b6bJqFjuXdc6%2F1aFD5KchVo%3D"
-$templateParameterUri = "https://zhenzhupload.blob.core.windows.net/fileupload/generated.parameters.json?st=2019-12-19T14%3A57%3A51Z&se=2020-12-20T14%3A57%3A00Z&sp=rl&sv=2018-03-28&sr=b&sig=sTolm78JJXqcUpH8a%2F2IldUq2O%2BEC5UF5dJ5AEq%2B8VU%3D"
-$resourceGroupName = "zhenzh-arm-demo1"
+$templateFile = "$env:ARTIFACT_DIR\$env:PRIMARY_ARTIFACT_SOURCE_ALIAS\template.json"
+$templateParameterFile = "$env:ARTIFACT_DIR\$env:PRIMARY_ARTIFACT_SOURCE_ALIAS\generated.parameters.json"
+$resourceGroupName = "zhenzh-arm-demo"
 $location = "West Europe"
+
+Write-Host "Content in directory: $env:ARTIFACT_DIR"
+Get-ChildItem -Path $env:ARTIFACT_DIR -Depth 4
 
 New-AzResourceGroup `
     -Name $resourceGroupName `
@@ -10,6 +13,6 @@ New-AzResourceGroup `
 
 New-AzResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
-    -TemplateUri $templateUri `
-    -TemplateParameterUri $templateParameterUri `
+    -TemplateFile $templateFile `
+    -TemplateParameterFile $templateParameterFile `
     -Force
