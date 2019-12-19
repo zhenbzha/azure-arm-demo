ab94462042b4a856add0f5356786ccc66aab0627
Zhenbo Zhang <zhenbo.zhang@microsoft.com>
2019-12-19 21:43:03 +0200

    test14

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,7 @@
 Write-Host "hello world"
 Write-Host "No problem reading $env:ARTIFACT_DIR"
-$templateFile = "$(System.ArtifactsDirectory)\Drop\azure-arm-demo\template.json"
-$templateParameterFile = "$(System.ArtifactsDirectory)\Drop\azure-arm-demo\generated.parameters.json"
+$templateFile = "$env:ARTIFACT_DIR\Drop\azure-arm-demo\template.json"
+$templateParameterFile = "$env:ARTIFACT_DIR\Drop\azure-arm-demo\generated.parameters.json"
 $resourceGroupName = "zhenzh-arm-demo1"
 $location = "West Europe"
 
