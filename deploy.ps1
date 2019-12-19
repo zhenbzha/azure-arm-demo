ef21f3f04e3b962ea0ff57a1cd3b0d3d1b3e0b87
Zhenbo Zhang <zhenbo.zhang@microsoft.com>
2019-12-19 21:24:34 +0200

    test11

--- deploy.ps1
+++ deploy.ps1
@@ -1,9 +1,9 @@
+Write-Host "This works!"
+
 $templateFile = "$(System.ArtifactsDirectory)\Drop\azure-arm-demo\template.json"
 $templateParameterFile = "$(System.ArtifactsDirectory)\Drop\azure-arm-demo\generated.parameters.json"
 $resourceGroupName = "zhenzh-arm-demo1"
 $location = "West Europe"
-
-Write-Host "This works: $(templateFile)"
 
 New-AzResourceGroup `
     -Name $resourceGroupName `
