3257c7696b1fb9049ad0257a17df16088d10e18b
Zhenbo Zhang <zhenbo.zhang@microsoft.com>
2019-12-19 21:51:06 +0200

    test17

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 Write-Host "hello world"
 Write-Host "No problem reading $env:ARTIFACT_DIR"
-Get-ChildItem -Path $env:ARTIFACT_DIR
+Get-ChildItem -Path $env:ARTIFACT_DIR -Depth 4
 Write-Host "hello world"
 
 $templateFile = "$env:ARTIFACT_DIR\Drop\azure-arm-demo\template.json"
