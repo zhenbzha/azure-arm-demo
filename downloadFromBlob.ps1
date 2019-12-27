134ec2ae391a5e057e0b132b899aea6d6bf4a14d
Zhenbo Zhang <zhenbo.zhang@microsoft.com>
2019-12-27 23:08:13 +0200

    user powershell param to define downloading folder

--- downloadFromBlob.ps1
+++ downloadFromBlob.ps1
@@ -1,10 +1,14 @@
+Param(
+    [parameter(mandatory=$true)][string]$baseDir
+)
+
 $resourceGroupName = "zhenzh-arm-demo-rg"
 $storageAccountName = "zhenzharmsa"
 $containerName = "attachments"
 $blobName = "user_input.txt"
-$destinationDir = "parameter_template"
+$destinationDir = "parameter_templates"
 
 $storageAccount = Get-AzStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroupName
 $context = $storageAccount.context
 #$sasToken = New-AzStorageContainerSASToken -Context $context -Container $containerName -Permission rwdl -ExpiryTime (get-date).AddHours(1)
-Get-AzStorageBlobContent  -Container $containerName -Blob $blobName -Destination $destinationDir -Context $context -Force
+Get-AzStorageBlobContent  -Container $containerName -Blob $blobName -Destination "$baseDir/$destinationDir/$blobName" -Context $context -Force
